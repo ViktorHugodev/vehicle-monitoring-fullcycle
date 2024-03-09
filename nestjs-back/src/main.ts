b598f7fda1498591719a1e87de6ee8317016d1f9
viktorhugodev <viktorhugo.dev@gmail.com>
2024-03-09 10:12:26 -0300

    feat: add functions to fetch source and destination

--- nestjs-back/src/main.ts
+++ nestjs-back/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = 3333;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
 bootstrap();
