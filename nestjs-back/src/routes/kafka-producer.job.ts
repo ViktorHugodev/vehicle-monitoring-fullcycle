665d4a1d1d0837109e69ed20f5eb9aa94fee9491
viktorhugodev <viktorhugo.dev@gmail.com>
2024-03-15 08:23:51 -0300

    feat: Update Docker Compose and NestJS code to use internal and external ports for Kafka and Control Center, and add Kafka producer to emit events when routes are moved or started.

--- nestjs-back/src/routes/kafka-producer.job.ts
+++ nestjs-back/src/routes/kafka-producer.job.ts
@@ -12,6 +12,7 @@ export class RouteKafkaProducerJob {
 
   @Process()
   async handle(job: Job<any>) {
+    console.log('🚀 ~ RouteKafkaProducerJob ~ handle ~ job:', job);
     await this.kafkaService.emit('route', job.data);
     return {};
   }
--- nestjs-back/src/routes/routes-driver/routes-driver.service.ts
+++ nestjs-back/src/routes/routes-driver/routes-driver.service.ts
@@ -1,4 +1,6 @@
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
 import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 interface CreateOrUpdateDto {
@@ -9,7 +11,10 @@ interface CreateOrUpdateDto {
 
 @Injectable()
 export class RoutesDriverService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    @InjectQueue('kafka-producer') private kafkaProducerQueue: Queue,
+  ) {}
 
   async createOrUpdate(dto: CreateOrUpdateDto) {
     // Tente encontrar um RouteDriver com base no route_id
@@ -27,24 +32,45 @@ export class RoutesDriverService {
         created_at: new Date(),
       });
 
-      return this.prismaService.routeDriver.update({
+      const routeDriver = await this.prismaService.routeDriver.update({
         where: { id: existingRouteDriver.id },
         data: { points: JSON.stringify(updatedPoints) },
       });
+      await this.kafkaProducerQueue.add({
+        event: 'RouteMoved',
+        id: routeDriver.id,
+        name: routeDriver.route_id,
+        started_at: new Date(),
+        lat: dto.lat,
+        lng: dto.lng,
+      });
+      return routeDriver;
     } else {
       // Se não existir, crie um novo RouteDriver...
       console.log('não existe route driver');
+
       const initialPoints = [
         { location: { lat: dto.lat, lng: dto.lng }, created_at: new Date() },
       ];
 
-      return this.prismaService.routeDriver.create({
+      const routeDriver = await this.prismaService.routeDriver.create({
         data: {
           route: { connect: { id: dto.route_id } },
           points: JSON.stringify(initialPoints),
           status: 'iniciado',
         },
       });
+
+      await this.kafkaProducerQueue.add({
+        event: 'RouteStarted',
+        id: routeDriver.id,
+        name: routeDriver.route_id,
+        started_at: new Date(),
+        lat: dto.lat,
+        lng: dto.lng,
+      });
+
+      return routeDriver;
     }
   }
 }
